2b83da79ca894d8bd6cb887fb9384564babcc69e
Macarena Haspert <hmacarena@gmail.com>
2019-07-08 18:38:02 -0300

    change character sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private bool isGrounded;
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0.0f)
+        {
+            Vector3 velocity = new Vector3(speed * horizontal * Time.deltaTime, 0.0f, 0.0f);
+            rb.MovePosition(transform.position + velocity);
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        if (horizontal < 0.0f)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+    }
+}
